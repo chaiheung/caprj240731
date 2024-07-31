41609ae9ab83efd31a327b372485856e148b6697
chaiheung <chaiheung@naver.com>
2024-07-31 17:17:01 +0900

    first commit

---
+++ be240731/src/main/java/com/example/be240731/Be240731Application.java
@@ -0,0 +1,13 @@
+package com.example.be240731;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Be240731Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Be240731Application.class, args);
+    }
+
+}
---
+++ be240731/src/test/java/com/example/be240731/Be240731ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be240731;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Be240731ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
