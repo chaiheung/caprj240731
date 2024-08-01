575e0ccee0e69b30c2ae9cf0f9ffc6110af64858
chaiheung <chaiheung@naver.com>
2024-08-01 12:20:10 +0900

    front: App.jsx / Home.jsx 경로 설정

--- fe240731/src/App.jsx
+++ fe240731/src/App.jsx
@@ -1,11 +1,27 @@
-function App() {
-  const [count, setCount] = useState(0)
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./page/Home.jsx";
+import { BoardWrite } from "./page/board/BoardWrite.jsx";
+import { BoardList } from "./page/board/BoardList.jsx";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      { path: "board/write", element: <BoardWrite /> }, // 게시판 글쓰기
+      { path: "board/list", element: <BoardList /> }, // 게시판 목록
+    ],
+  },
+]);
+
+function App(props) {
   return (
-    <>
-      <div>hello</div>
-    </>
-  )
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ fe240731/src/page/board/BoardList.jsx
@@ -0,0 +1,6 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+export function BoardList() {
+  return <Box>글 목록 화면</Box>;
+}
