bf0d9fcadde37d161561008764f1c715941a5ffa
chaiheung <chaiheung@naver.com>
2024-08-01 15:34:48 +0900

    back: BoardList/BoardView

--- fe240731/src/App.jsx
+++ fe240731/src/App.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./page/Home.jsx";
 import { BoardWrite } from "./page/board/BoardWrite.jsx";
 import { BoardList } from "./page/board/BoardList.jsx";
+import { BoardView } from "./page/board/BoardView.jsx";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
     children: [
       { path: "board/write", element: <BoardWrite /> }, // 게시판 글쓰기
       { path: "board/list", element: <BoardList /> }, // 게시판 목록
+      { path: "board/:id", element: <BoardView /> }, // 게시글 보기
     ],
   },
 ]);
