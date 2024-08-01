c2766c92640e3a14bb8067c9c86816d836fafa8e
chaiheung <chaiheung@naver.com>
2024-08-01 14:38:54 +0900

    back: BoardWrite 프론트엔드 기본로직 작성

--- fe240731/src/page/board/BoardWrite.jsx
+++ fe240731/src/page/board/BoardWrite.jsx
@@ -1,6 +1,98 @@
-import { Box } from "@chakra-ui/react";
-import React from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  useToast,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function BoardWrite() {
-  return <Box>글 작성 화면</Box>;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [writer, setWriter] = useState("");
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  function handleSaveClick() {
+    setLoading(true);
+    axios
+      .post("/api/board/create", {
+        title,
+        content,
+        writer,
+      })
+      .then(() => {
+        toast({
+          description: "새 글이 등록되었습니다.",
+          status: "success",
+          position: "top",
+        });
+        navigate("/");
+      })
+      .catch((e) => {
+        const code = e.response.status;
+
+        if (code === 400) {
+          toast({
+            status: "error",
+            description: "등록되지 않았습니다. 입력한 내용을 확인하세요.",
+            position: "top",
+          });
+        }
+      })
+      .finally(() => setLoading(false));
+  }
+
+  let disableSaveButton = false;
+  if (title.trim().length === 0) {
+    disableSaveButton = true;
+  }
+  if (content.trim().length === 0) {
+    disableSaveButton = true;
+  }
+  if (writer.trim().length === 0) {
+    disableSaveButton = true;
+  }
+
+  return (
+    <Box>
+      <Box>글 작성 화면</Box>
+      <Box>
+        <Box>
+          <FormControl>
+            <FormLabel>제목</FormLabel>
+            <Input onChange={(e) => setTitle(e.target.value)} />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>본문</FormLabel>
+            <Textarea onChange={(e) => setContent(e.target.value)} />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>작성자</FormLabel>
+            <Input onChange={(e) => setWriter(e.target.value)} />
+          </FormControl>
+        </Box>
+        <Box>
+          <Button
+            isLoading={loading}
+            isDisabled={disableSaveButton}
+            colorScheme={"blue"}
+            onClick={handleSaveClick}
+          >
+            저장
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
